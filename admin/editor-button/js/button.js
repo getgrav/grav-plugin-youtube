ea72041f516ed74918bdb9762e787e41413d9b6e
Flavio Copes <copesc@gmail.com>
2016-04-28 17:18:18 +0200

    Re-apply Update button for the new Admin 1.1
    https://github.com/getgrav/grav-plugin-youtube/commit/58fbb51b110006a52c
    1bf4845b789468d3fe3919

--- admin/editor-button/js/button.js
+++ admin/editor-button/js/button.js
@@ -1,31 +1,35 @@
-(function(){
+(function($){
+    $(function(){
+        $('body').on('grav-editor-ready', function() {
+            var Instance = Grav.default.Forms.Fields.EditorField.Instance;
+            Instance.addButton({
+                notices: {
+                    identifier: 'youtube-video',
+                    title: 'YouTube Video',
+                    label: '<i class="fa fa-fw fa-youtube"></i>',
+                    modes: ['gfm', 'markdown'],
+                    action: function({ codemirror, button }) {
+                        button.on('click.editor.notice-info', function() {
+                            var videoURL = prompt("Enter the YouTube Video URL. E.g. https://www.youtube.com/watch?v=vQ4qK36UenI");
 
-    if (typeof window.customToolbarElements === 'undefined') { window.customToolbarElements = []; }
-    window.customToolbarElements.push({
-        identifier: 'youtube-video',
-        button: {
-            title  : 'YouTube Video',
-            label  : '<i class="fa fa-fw fa-youtube"></i>',
-        },
-        processAction: function(editor) {
-            var videoURL = prompt("Enter the YouTube Video URL. E.g. https://www.youtube.com/watch?v=Q1M5f6mjsac");
+                            if (videoURL) {
+                                var text = '[plugin:youtube](' + videoURL + ')';
 
-            if (videoURL) {
-                var text = '[plugin:youtube](' + videoURL + ')';
+                                //Add text to the editor
+                                var pos     = codemirror.getDoc().getCursor(true);
+                                var posend  = codemirror.getDoc().getCursor(false);
 
-                //Add text to the editor
-                var cm      = editor.editor,
-                    pos     = cm.getDoc().getCursor(true),
-                    posend  = cm.getDoc().getCursor(false);
+                                for (var i=pos.line; i<(posend.line+1);i++) {
+                                    codemirror.replaceRange(text+codemirror.getLine(i), { line: i, ch: 0 }, { line: i, ch: codemirror.getLine(i).length });
+                                }
 
-                for (var i=pos.line; i<(posend.line+1);i++) {
-                    cm.replaceRange(text+cm.getLine(i), { line: i, ch: 0 }, { line: i, ch: cm.getLine(i).length });
+                                codemirror.setCursor({ line: posend.line, ch: codemirror.getLine(posend.line).length });
+                                codemirror.focus();
+                            }
+                        });
+                    }
                 }
-
-                cm.setCursor({ line: posend.line, ch: cm.getLine(posend.line).length });
-                cm.focus();
-            }
-        }
+            });
+        });
     });
-
-})();
+})(jQuery);
