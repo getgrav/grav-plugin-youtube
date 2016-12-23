ca493505996e28ee29bd8d5a73326bc1221aa43b
iusvar <severoiuliano@gmail.com>
2016-12-23 10:58:09 +0100

    fix property undefined (#16)

--- admin/editor-button/js/button.js
+++ admin/editor-button/js/button.js
@@ -3,13 +3,14 @@
         $('body').on('grav-editor-ready', function() {
             var Instance = Grav.default.Forms.Fields.EditorField.Instance;
             Instance.addButton({
-                notices: {
+                youtube: {
                     identifier: 'youtube-video',
                     title: 'YouTube Video',
                     label: '<i class="fa fa-fw fa-youtube"></i>',
                     modes: ['gfm', 'markdown'],
-                    action: function({ codemirror, button }) {
-                        button.on('click.editor.notice-info', function() {
+                    action: function(_ref) {
+                        var codemirror = _ref.codemirror, button = _ref.button, textarea = _ref.textarea;
+                        button.on('click.editor.youtube',function() {
                             var videoURL = prompt("Enter the YouTube Video URL. E.g. https://www.youtube.com/watch?v=vQ4qK36UenI");
 
                             if (videoURL) {
