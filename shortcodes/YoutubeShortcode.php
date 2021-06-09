d4d3557129947a7ec31417db88c52971edd6fa53
Angus <angusmcb@gmail.com>
2021-06-09 12:41:26 +0200

    Just some extra comments

--- shortcodes/YoutubeShortcode.php
+++ shortcodes/YoutubeShortcode.php
@@ -34,6 +34,8 @@ class YoutubeShortcode extends Shortcode
                     $page_media = $this->grav['page']->media();
 
                     if (isset($page_media[$custom_thumbnail])) {
+                        // Get the url of the thumbnail.
+                        // No resizing takes (the best size is not known) place so there is a potential performance problem here if the user specifies a very large image!
                         $custom_thumbnail_url = $page_media[$custom_thumbnail]->url();
                     }
                 }
